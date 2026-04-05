3c1d19ed87b79ad3c905457e8b5583c538a6707d
see96385221-ai <see96385221@gmail.com>
2026-04-06 00:18:08 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
-  images: { unoptimized: true }
+  images: {
+    unoptimized: true
+  }
 }
+
 module.exports = nextConfig
